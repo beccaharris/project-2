e212e6296e2fc954fb74b18e5b4724c52b0d7988
Phoenix Werth <phoenixwerth@Phoenixs-MacBook-Pro.local>
2018-05-21 21:11:13 -0600

    Survey page

--- models/dog.js
+++ models/dog.js
@@ -4,5 +4,40 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
+    temperment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    size: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    activity_level: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    barking_level: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    hypoallergenic: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false
+    },
+    good_for_kids: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false
+    },
+    shedding: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    trainability: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   })
+  return Dog;
 }
