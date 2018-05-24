6fdc60bb197de63a2285a9af09652190d3a70636
Wyatt Sommer <wyatt@hampton.digital>
2018-05-23 19:52:18 -0600

    wyatt3

--- app/routes/api-routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require("path");
-
-var app = express();
-var port = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,9 +4,9 @@ var path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/survey");
@@ -16,8 +16,13 @@ module.exports = function(app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/survey", isAuthenticated, function(req, res) {
+  app.get("/survey", isAuthenticated, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  // survey answers page
+  app.get("/answer", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/answer.html"));
   });
 
 };
